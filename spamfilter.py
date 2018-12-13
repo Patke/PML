e0dd3420e5100c0ab3c2738b3ab308b4b8bdffa2
Patke <1611037@stud.hs-mannheim.de>
2018-12-13 12:09:10 +0100

    weitergehts

--- spamfilter.py
+++ spamfilter.py
@@ -1,22 +1,16 @@
 import os
 from nltk.classify import NaiveBayesClassifier
-import sklearn.naive_bayes
-from random import random
 import random
 import nltk
-nltk.download('punkt')
 from collections import Counter
-import re
-from collections import Counter
-import numpy as np
-from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
-from sklearn.feature_extraction.text import CountVectorizer
+from Parameter import *
 
-from sklearn.svm import SVC, NuSVC, LinearSVC
-from sklearn.metrics import confusion_matrix
+nltk.download('punkt')
 
 # Funktionen definieren
 rootdir = "C:\\Users\\Kevin\\PycharmProjects\\PML\\Spamfilter.Template"
+dir_noSpam = "C:\\Users\\Kevin\\PycharmProjects\\PML\\Spamfilter.Template\\dir.nospam"
+dir_Spam = "C:\\Users\\Kevin\\PycharmProjects\\PML\\Spamfilter.Template\\dir.spam"
 
 
 def directoryliste(pfad, filter):
@@ -29,7 +23,8 @@ def directoryliste(pfad, filter):
 
 print(directoryliste(rootdir, filter))
 
-
+nsp_dict = []
+spam_dict = []
 nsp_list = []
 spam_list = []
 
@@ -39,26 +34,67 @@ def create_word_features(words):
     return my_dict
 
 
+char_replaceList = list(char_replaces.keys())
+print(char_replaceList)
+print(char_replaceList[1])
 for directories, subdirs, files in os.walk(rootdir):
-    if (os.path.split(directories)[1]  == 'dir.nospam'):
+    if (os.path.split(directories)[1] == 'dir.nospam'):
         for filename in files:
             with open(os.path.join(directories, filename)) as f:
-                data = f.read()
+                    data = f.read()
                 # The data we read is one big string. We need to break it into words.
-                words = nltk.word_tokenize(data)
-                nsp_list.append((create_word_features(words), "dir.nospam"))
+                    words = data.split()
+                    nsp_dict.append((create_word_features(words), "dir.nospam"))
+            for i in words:
+                nsp_list.append(i)
+                # for i in words:
+                #     if "@" in i:
+                #         print("Thats the right row: " + i)
+
     if (os.path.split(directories)[1] == 'dir.spam'):
         for filename in files:
             with open(os.path.join(directories, filename)) as f:
-                data = f.read()
-                words = nltk.word_tokenize(data)
-                spam_list.append((create_word_features(words), "dir.nospam"))
+                    data = f.read()
+                    words = data.split()
+                    spam_dict.append((create_word_features(words), "dir.spam"))
+            for i in words:
+                spam_list.append(i)
 
-print(nsp_list[0])
-print(spam_list[0])
-# print(spam_list[0])
+print("test")
+print(nsp_list)
+print(spam_list)
 
-combined_list = nsp_list + spam_list
+list_to = nsp_list + spam_list
+
+dict_countSpam = {}
+dict_countNoSpam = {}
+dict_countAll = {}
+for word in list_to:
+    dict_countAll[word] = dict_countAll.get(word, 0) + 1
+for word in nsp_list:
+    dict_countNoSpam[word] = dict_countNoSpam.get(word, 0) + 1
+for word in spam_list:
+    dict_countSpam[word] = dict_countSpam.get(word, 0) + 1
+
+print("test2")
+print(dict_countAll)
+print(dict_countNoSpam)
+print(dict_countSpam)
+print(Counter(list_to).most_common())
+# print(nsp_dict[0])
+# print(spam_dict[0])
+
+# nsp = list(nsp_dict)
+# print("test")
+# print(nsp)
+# d = {}
+# for word in nsp:
+#     d[word] = d.get(word, 0) + 1
+# print("ich bin krass: " + d)
+# # print(spam_list[0])
+
+
+combined_list = nsp_dict + spam_dict
 print(len(combined_list))
 random.shuffle(combined_list)
 
@@ -70,18 +106,31 @@ test_set = combined_list[training_part:]
 print(len(training_set))
 print(len(test_set))
 
-classifier = NaiveBayesClassifier.train(training_set)
+classifier = NaiveBayesClassifier.train(combined_list)
 
 accuracy = nltk.classify.util.accuracy(classifier, test_set)
 
 print("Accuracy is: ", accuracy)
 
-print(classifier.show_most_informative_features())
+print(classifier.show_most_informative_features(100))
 
 
+# inputPath = "C:\\Users\\Kevin\\PycharmProjects\\PML\\Spamfilter.Template\\dir.mail.input"
+# for directories, subdirs, files in os.walk(inputPath):
+#     if (os.path.split(directories)[1] == 'dir.mail.input'):
+#         for filename in files:
+#             with open(os.path.join(directories, filename)) as f:
+#                 input_text = f.read()
+#
+# print("That's the inputtext: " + input_text)
+#
+# words = nltk.word_tokenize(input_text)
+# features = create_word_features(words)
+# print("Message 1 is :" + classifier.classify(features))
 
 
-
+# print(training_set)
+# print(spam_list)
 
 
 def maildatei_laden(dateiname):
